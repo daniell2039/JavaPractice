9d6dff7371898ee3855cdacbde6153bb0329b320
Daniel Leon <danirl.99.117@gmail.com>
2023-10-12 14:33:41 -0600

    create customers

--- JavaPOO/src/POO/Customer.java
+++ JavaPOO/src/POO/Customer.java
@@ -9,16 +9,55 @@ public class Customer {
 
     double moneyAmount;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public double getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(double moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
+
     // Create the constructor, using the previous declared properties and using this. creating the parameters for the objects
     public Customer(int id, int age, String name, String lastname, double moneyAmount) {
         this.id = id;
         this.age = age;
         this.name = name;
         this.lastname = lastname;
-        this.moneyAmount = 0;
-    }
+        this.moneyAmount = moneyAmount;
 
-    public void getName () {
+
 
     }
 
--- JavaPOO/src/POO/Customers.java
+++ JavaPOO/src/POO/Customers.java
@@ -2,5 +2,13 @@ package POO;
 
 public class Customers {
 
-    Customer customer1 = new Customer (13213, 38, "Daniel", "Leon", 100000);
+    public static void main(String[] args) {
+
+        Customer customer1 = new Customer(13213, 38, "Daniel", "Leon", 100000);
+
+        // Now with the getters and setters we can have the customer specific data, for example this :
+        System.out.println("Customer 1 tiene la cantidad de " + customer1.getMoneyAmount() + " En su cuenta");
+        System.out.println("El nombre de customer 1 es " + customer1.getName());
+        System.out.println("El apellido de customer 1 es " + customer1.getId());
+    }
 }
